2241dd3a6fdf7c0fe07bc4bff26fd3670fcee0bd
Vanessa <vahnkoch@gmail.com>
2021-07-02 13:22:58 -0300

    Importando middlewares

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username)
+
+  if(!user) {
+    return response.status(404).json({ error: 'Usuário não encontrado.'});
+  }
+
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if((!user.pro && user.todos.length < 10) || user.pro) {
+    return next();
+  } else {
+    return response.status(403).json({ error: 'Limite de todo atingido'});
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const isIdValid = validate(id);
+
+  if(isIdValid) {
+    const user = users.find(user => user.username === username)
+    if(!user) {
+      return response.status(404).json({ error: 'User não existe.'})
+    }
+
+    const todo = user.todos.find(todo => todo.id === id);
+  
+    if(!todo) {
+      return response.status(404).json({ error: 'Todo não existe.'})
+    }
+    
+    request.user = user;
+    request.todo = todo;
+    return next();
+  } else {
+    return response.status(400).json({ error: 'UUID inválido.'})
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id)
+
+  if(!user) {
+    return response.status(404).json({ error: 'Usuário não encontrado.'});
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
